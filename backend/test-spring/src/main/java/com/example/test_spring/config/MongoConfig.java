5def7bb27a12c90c3c7bdb5a64be7b852b6fe02a
P. Muni Lokitha <luckylokitha@gmail.com>
2025-12-16 15:09:59 +0530

    SECURITY: Hide MongoDB credentials using environment variables

--- backend/test-spring/src/main/java/com/example/test_spring/config/MongoConfig.java
+++ backend/test-spring/src/main/java/com/example/test_spring/config/MongoConfig.java
@@ -2,6 +2,7 @@ package com.example.test_spring.config;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -10,17 +11,32 @@ import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 @Configuration
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
+    @Value("${mongodb.database:resolveIT_db}")
+    private String databaseName;
+    
+    @Value("${mongodb.username:resolveIT_db_user}")
+    private String username;
+    
+    @Value("${mongodb.password:your_password_here}")
+    private String password;
+    
+    @Value("${mongodb.cluster:cluster0.eqvpfku.mongodb.net}")
+    private String cluster;
+
     @Override
     protected String getDatabaseName() {
-        return "resolveIT_db";
+        return databaseName;
     }
 
     @Bean
     @Primary
     @Override
     public MongoClient mongoClient() {
-        String connectionString = "mongodb+srv://resolveIT_db_user:lokitha2005@cluster0.eqvpfku.mongodb.net/resolveIT_db?retryWrites=true&w=majority&appName=Cluster0";
-        System.out.println("Creating MongoDB client with Atlas connection: " + connectionString);
+        String connectionString = String.format(
+            "mongodb+srv://%s:%s@%s/%s?retryWrites=true&w=majority&appName=Cluster0",
+            username, password, cluster, databaseName
+        );
+        System.out.println("Creating MongoDB client with Atlas connection");
         return MongoClients.create(connectionString);
     }
 }
